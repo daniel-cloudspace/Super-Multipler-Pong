916d7894496eb8e96896f0ae2d391bdb81baf64c
Daniel <daniel@cloudspace.com>
2011-06-08 17:43:49 -0400

    shared ball maybe this will work?

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var sys = require("sys");
 var util = require('util');
 var io = require("socket.io");
 
+const DOM_VK = { LEFT:37, UP:38, RIGHT:39, DOWN:40 };
 
 app = express.createServer();
 
@@ -24,27 +25,88 @@ var ball = { x:100, y:300, angle: 0.5, speed: 10 };
 var event_buffer = {};
 var time;
 
+function new_player() {
+    var starter_x = ( Math.random() > 0.5 ? 100 : 900 );
+    var keystrokes = {};
+    keystrokes[DOM_VK.UP] = keystrokes[DOM_VK.DOWN] = keystrokes[DOM_VK.RIGHT] = keystrokes[DOM_VK.LEFT] = false;
+    return { x: starter_x, y: 0, updated: false, keystrokes: keystrokes}
+}
+
+function game_tick() {
+    ball.x += ball.speed * Math.cos(ball.angle);
+    ball.y += ball.speed * Math.sin(ball.angle);
+    b = ball.y - Math.tan(ball.angle) * ball.x;
+
+    // animate all players according to their keystrokes
+    for (i in players) {
+        if (typeof players[i].keystrokes != 'undefined') {
+            if (players[i].keystrokes[DOM_VK.UP])    { players[i].y -= ponger_step; }
+            if (players[i].keystrokes[DOM_VK.DOWN])  { players[i].y += ponger_step; }
+            if (players[i].keystrokes[DOM_VK.RIGHT]) { players[i].x += ponger_step; }
+            if (players[i].keystrokes[DOM_VK.LEFT])  { players[i].x -= ponger_step; }
+        }
+        
+        // check for collision with ball
+        if (ball.y < players[i].y + 120 && ball.y > players[i].y) {
+            var diff = ball.x - players[i].x;
+            if (players[i].x < 500 && Math.cos(ball.angle) < 0 && diff > 0 && diff < 10) {
+                ball.angle = Math.PI - ball.angle;
+                ball.x = players[i].x + diff;
+            } else if (players[i].x > 500 && Math.cos(ball.angle) > 0 && diff > -10 && diff < 0) {
+                ball.angle = Math.PI - ball.angle;
+                ball.x = players[i].x - diff;
+            }
+        }
+    }
+
+    if (ball.y < 0) {
+        ball.angle = 0 - ball.angle;
+        ball.y = Math.abs(ball.y);
+    } else if (ball.y > 500) {
+        ball.angle = 0 - ball.angle;
+        ball.y = 1000 - ball.y;
+    }
+    if (ball.x < 0) {
+        ball.angle = Math.PI - ball.angle;
+        ball.x = Math.abs(ball.x);
+    } else if (ball.x > 1000) {
+        ball.angle = Math.PI - ball.angle;
+        ball.x = 2000 - ball.x;
+    }
+}
+
 socket.on('connection', function(client) {
-  client.send({ init_data: { your_id: client.sessionId, ball: ball, server_time: new Date().getTime() } });
+  players[client.sessionId] = new_player(); 
+
+  client.send({ init_data: { your_id: client.sessionId, your_player: players[client.sessionId], ball: ball, server_time: new Date().getTime() } });
 
   client.on('message', function(message){
     event_buffer[message.my_id] = message.the_event;
+    players[message.my_id].x = message.the_event.x;
+    players[message.my_id].y = message.the_event.y;
+    players[message.my_id].keystrokes[DOM_VK.UP] = message.the_event.keystrokes[DOM_VK.UP];
+    players[message.my_id].keystrokes[DOM_VK.DOWN] = message.the_event.keystrokes[DOM_VK.DOWN];
+    players[message.my_id].keystrokes[DOM_VK.LEFT] = message.the_event.keystrokes[DOM_VK.LEFT];
+    players[message.my_id].keystrokes[DOM_VK.RIGHT] = message.the_event.keystrokes[DOM_VK.RIGHT];
   });
 
   client.on('disconnect', function(){ sys.puts("client disconnected"); });
 });
+
 var count=0;
 var time = new Date().getTime();
+var ponger_step = 10, half_window = 500;
 setInterval(function() {
     // this is basically: if event_buffer is not empty:
     for (i in event_buffer) {
-        socket.broadcast({
-            time: new Date().getTime(), 
-            events: event_buffer
-        });
+        var update_data = { time: new Date().getTime(), events: event_buffer };
+        if (count%10) update_data.ball = ball;
+        socket.broadcast(update_data);
         event_buffer = {};
         break;
     }
+
+    game_tick();
+
     count = count +1;
-    console.log(count + "   " + (new Date().getTime() - time)/50);
 }, 50);
